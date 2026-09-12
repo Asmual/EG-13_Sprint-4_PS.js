eae73f1c2be692e063c42d2f694297a551f8d8e6
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-12 13:16:10 +0600

    added wordPattern solution with test case

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -26,3 +26,36 @@ var isIsomorphic = function(s, t) {
 
 // console.log(isIsomorphic("egg", "add"));
 // Expected Output: true
+
+/********** 02. Word Pattern **********/
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    const words = s.split(" ");
+    if (pattern.length !== words.length) return false;
+
+    const charToWord = {};
+    const wordToChar = {};
+
+    for (let i = 0; i < pattern.length; i++) {
+        const char = pattern[i];
+        const word = words[i];
+
+        if ((charToWord[char] && charToWord[char] !== word) || (wordToChar[word] && wordToChar[word] !== char)) {
+            return false;
+        }
+
+        charToWord[char] = word;
+        wordToChar[word] = char;
+    }
+
+    return true;
+};
+
+// console.log(wordPattern("abba", "dog cat cat dog"));
+// Expected Output: true
+
+
