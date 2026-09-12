ef7fcbda3c8f91c24b325390758f12a641e493bb
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-12 13:19:50 +0600

    added productExceptSelf solution with test case

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -125,3 +125,29 @@ var middleNode = function(head) {
 // console.log(middleNode(list5));
 // Expected Output: ListNode { val: 3, next: ListNode { val: 4, ... } }
 
+/********** 06. Product of Array Except Self **********/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const n = nums.length;
+    const output = new Array(n).fill(1);
+
+    let leftProduct = 1;
+    for (let i = 0; i < n; i++) {
+        output[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+
+    let rightProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        output[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return output;
+};
+
+// console.log(productExceptSelf([1, 2, 3, 4]));
+// Expected Output: [24, 12, 8, 6]
